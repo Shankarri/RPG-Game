f3ae0fc9eb900d705cf4097f891e38b194e6c64d
Shanki <shanki.ranju@gmail.com>
2019-02-01 02:12:59 -0600

    completed half

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,71 +3,84 @@ $(document).ready(function () {
     var battlesound = $("#battle_Sound");
     var userattack = $("#user-attack");
     var enemyattack = $("#enemy-attack");
-    var attackingDone = true;
-
+    var attackingDone;
     var user = {
-        userSecId : "",
-        userName : "",
-        userHP :0 ,
-        userDamage : 0 
-    }
+        secId : "",
+        name : "",
+        HP : 0 ,
+        AP : 0,
+        Incrementer:0, 
+        damage : 0 
+    };
     var enemy = {
-        enemySecId : "",
-        enemyName : "",
-        enemyHP :0 ,
-        enemyDamage : 0 
-    } 
+        secId : "",
+        name : "",
+        HP :0 ,
+        AP : 0,
+        Incrementer:0, 
+        damage : 0 
+    } ;
+
+    function initialize()
+    {
+        attackingDone = true;
+        $("#restartBtn").hide();
+        $(".battle").hide();
+        $(".battlePoints").hide();
+        $(".header-msg").html( "Select your character");
+    }
+
+
+    
 
   
-    $("#restartBtn").hide();
-    $(".battle").hide();
-    $(".header-msg").html( "Select your character");
 
-    $("#characters-row section").on("click", function (event) {
-        // 
+
+    $("#characters-row section").on("click", function (event) { 
         $(".battle").show();
-        
-        var targetId =event.currentTarget.id;
-        var strsplit  = targetId.split("_");
-        var character = strsplit [0];
-        var charhp = parseInt(event.currentTarget.dataset.hp);
-        var chardamage = parseInt(event.currentTarget.dataset.attack);
-
-        if(user.userSecId =="") 
+        var targetId = event.currentTarget.id;
+        var charname = targetId.split("_")[0];
+        let character = {
+            secId : event.currentTarget.id,
+            name : charname,
+            Incrementer : parseInt(event.currentTarget.dataset.intialap),
+            HP :  parseInt($("#"+charname+"HP").text()),
+            AP :  parseInt($("#"+charname+"AP").text()),
+            damage : parseInt($("#"+charname+"Damage").text())
+        }
+        if(user.secId =="") 
         {
            $("#user-char").append(event.currentTarget);
-            user.userSecId = targetId;  
-            user.userName =character;
-            user.userHP = charhp;
-            user.userDamage = chardamage;
-
-           
+            user =character;
             console.log ("user",user);
             $(".header-msg").html( "Select your Enemy");
         }
-        else if((enemy.enemySecId == "") && (targetId !=user.userSecId))
+        else if((enemy.secId == "") && (targetId !=user.secId))
         {
             $("#enemy-char").append(event.currentTarget);
-            enemy.enemySecId = targetId;  
-            enemy.enemyName = character;
-            enemy.enemyHP = charhp;
-            enemy.enemyDamage = chardamage;
+            enemy = character;
             console.log ("enemy",enemy);
             $(".header-msg").html( "Press the Attack button to start fight");
         }
     });
     
+    $("#restartBtn").on("click", function (event) {
+        window.location.reload();
+        
+    });
     $("#attackBtn").on("click", function (event) {
         
-        if (enemy.enemySecId != "") {
-            enemy.enemyHP -= user.userDamage;
-            user.userHP-=enemy.enemyDamage;
-            user.userDamage +=user.userDamage; 
-
-            $("#"+user.userSecId).attr("hp",user.userHP )
-            $("#"+user.userSecId).attr("attack",user.userDamage )
-            console.log ("enemy",enemy);
-            console.log ("user",user);
+        if (enemy.secId != "" && enemy.HP>0 && user.HP>0) {
+            
+            enemy.HP -= user.AP;
+            enemy.damage +=user.AP;
+            user.HP -=enemy.AP;
+            user.damage +=enemy.AP;
+            user.AP +=user.Incrementer;
+            
+            if(enemy.HP <= 0)
+                {attackingDone = false;}
+            
             if (attackingDone) {
                 $(".header-msg").html("Keep pressing the attack button until enemy is defeated");
                 attackingDone = false;
@@ -75,15 +88,51 @@ $(document).ready(function () {
                 attack(enemyattack, "right");
                 setTimeout(stopattacking, 2000);
             }
+
+            updateCharacterData (user);
+            updateCharacterData (enemy);
         }
-        else
+        else if (enemy.secId == "")
         {
             alert("Select an enemy to attack");
         }
+        if(user.HP <= 0)
+        {
+            $(".header-msg").html( "<h3>You have been defeated. Please press the restart Button to start a new game<h3>");
+            $("#enemy-char").empty();
+            $("#user-char").empty();
+            $("#restartBtn").show();
+        }
+         else if (enemy.HP <= 0 && enemy.secId != "")
+        {
+            $(".header-msg").html( "You have defected the enemy. Select Next defender");
+            $("#enemy-char").empty();
+            enemy = {
+                secId : "",
+                name : "",
+                HP :0 ,
+                AP : 0,
+                Incrementer:0, 
+                damage : 0 
+            };
+            if($("#characters-row section").length == 0)
+            {
+                $(".header-msg").html( "<h3>You are the winner. You have defected all the enemies  Please press the restart Button to start a new game<h3>");
+                $("#enemy-char").empty();
+                $("#user-char").empty();
+                $("#restartBtn").show();
+            }
+        }
+     });
 
-       
-    });
-
+    function updateCharacterData(character)
+    {
+            $("#"+character.name+"HP").text(character.HP);
+            $("#"+character.name+"AP").text(character.AP);
+            $("#"+character.name+"Damage").text(character.damage);
+            $("#"+character.secId+" .battlePoints").show();
+            console.log ("character",character);
+    }
     function stopattacking()
     {
 
@@ -101,13 +150,13 @@ $(document).ready(function () {
         
         if(position == "left")
         {
-            charName = user.userName;
+            charName = user.name;
             battleImg.attr("id", "battle_img_1");
             battleImg.attr("style", "height: 200px;width: 350px; padding-left:3%;-webkit-transform: scaleX(-1);transform: scaleX(-1); ");
         }
         else
         {
-            charName = enemy.enemyName;
+            charName = enemy.name;
             battleImg.attr("id", "battle_img_2");
             battleImg.attr("style", "height: 200px;width: 350px;padding-right:3%;");
         }
@@ -117,6 +166,8 @@ $(document).ready(function () {
 
         charattack.append(battleImg);
         charattack.show();
+
+
     }
 });
 
